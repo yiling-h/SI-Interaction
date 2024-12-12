39c167c4f745395e49ec4c2777d35383d6ba5087
yiling-h <yilingh@umich.edu>
2024-12-12 17:53:07 -0500

    Vary corr and vary main

---
+++ selectinf/Simulation/H1/SimulationNotebooks/vary_corr.py
@@ -0,0 +1,272 @@
+import time, sys, joblib, os
+sys.path.append('/home/yilingh/SI-Interaction')
+import multiprocessing as mp
+
+from selectinf.Simulation.H1.nonlinear_H1_helpers import *
+
+def predict(beta_hat, X_test):
+    return X_test.dot(beta_hat)
+
+def generate_test(Y_mean, noise_sd):
+    n = Y_mean.shape[0]
+    Y_test = Y_mean + np.random.normal(size=(n,), scale=noise_sd)
+
+    return Y_test
+
+def vary_corr(start, end, dir):
+    # A dictionary recording simulation results and metrics
+    oper_char = {}
+    oper_char["coverage rate"] = []
+    oper_char["avg length"] = []
+    oper_char["method"] = []
+    # oper_char["MSE"] = []
+    oper_char["prop"] = []
+    oper_char["rho"] = []
+    oper_char["signal"] = []
+    oper_char["main signal"] = []
+    oper_char["target"] = []
+    oper_char["power"] = []
+
+    MSE_dict = {}
+    MSE_dict["MSE"] = []
+    MSE_dict["method"] = []
+    MSE_dict["rho"] = []
+
+    # Dictionary of projected targets, over all simulation parameters
+    target_dict = {}
+    target_dict["parameter"] = []
+    target_dict["target"] = []
+    target_dict["target id"] = []
+    target_dict["method"] = []
+    target_dict["index"] = []
+    target_dict["pivot"] = []
+    target_dict["pval"] = []
+
+    # A dictionary recording p-values for each true interaction
+    # over all simulation results.
+    # Each simulation parameter (here parameter_list contain a list of main signal strengths
+    # to be considered) has a corresponding dictionary of results
+    parameter_list = np.array([0, 0.2, 0.4, 0.6])
+    pval_dict = {}
+    for x in parameter_list:
+        pval_dict[x] = {}
+        for m in ['Naive', 'Data Splitting', 'MLE']:
+            pval_dict[x][m] = []
+
+    # Group lasso solver constructor
+    const = group_lasso.gaussian
+    active_inter_list_true = np.array([[0, 1], [1, 2], [2, 4], [1, 5], [2, 6]])
+    active_inter_list_true_list = [(x[0], x[1]) for x in active_inter_list_true]
+
+    # p = 50
+    # rho = 0.5 # Correlation of signal covariates (amongst themselves), and noise.
+    sig = 2  # Controlling interaction vs main signals.
+    # Setting it this way generates comparable main
+    # and interaction signals (sig = 2 works )
+    weights = 0.05  # Group Lasso weights
+    s_inter = 5  # Number of true interactions
+    p_nl = 20  # Number of nonlinear covariates
+    n = 200
+    root_n_scaled = False
+    main_sig = 2
+    prop = 0.9
+
+    ds_rank_def_count = {corr: 0 for corr in parameter_list}
+
+    for rho in parameter_list:
+        for i in range(200):
+            np.random.seed(i + 1000)
+            # MSE_set = False
+
+            print(rho, i, "th simulation done")
+            print("seed:", i + 1000)
+            # Generating a (X, Y) pair, and corresponding basis expansion
+            # The 'weakhierarchy' argument is overridden by setting
+            # `active_inter_list`.
+            (design, data_interaction, Y, Y_mean, data_combined,
+             groups, active, active_inter_adj, active_inter_list, gamma) \
+                = (generate_gaussian_instance_nonlinear_interaction_simple
+                   (n=n, p_nl=p_nl, rho=rho, full_corr=False,
+                    rho_noise=rho, block_corr=True, rho_cross=rho * 0.8,
+                    SNR=None, main_signal=main_sig, noise_sd=5,
+                    nknots=6, degree=2, interaction_signal=sig,
+                    random_signs=False, scale=root_n_scaled, center=False,
+                    structure='weakhierarchy', s_interaction=s_inter,
+                    intercept=True, active_inter_list=active_inter_list_true,
+                    return_gamma=True))
+            print("SD(Y): ", np.std(Y))
+            Y_test = Y_mean + np.random.normal(size=(n,), scale=5)
+
+            # Performing Naive inference using 'all pairs'
+            (coverages, lengths, selected_inter, p_values, pivots, targets, idx,
+             beta_hat_naive) \
+                = naive_inference_inter(X=design, Y=Y, groups=groups,
+                                        Y_mean=Y_mean, const=const,
+                                        n_features=20, interactions=data_interaction,
+                                        weight_frac=weights, level=0.9, mode='weakhierarchy',
+                                        solve_only=False, continued=False,
+                                        parallel=False, p_val=True,
+                                        return_pivot=True, intercept=True,
+                                        target_ids=None,
+                                        root_n_scaled=root_n_scaled)
+
+            noselection_naive = coverages is None
+
+            if not noselection_naive:
+                # Naive
+                oper_char["coverage rate"].append(np.mean(coverages))
+                oper_char["avg length"].append(np.mean(lengths))
+                oper_char["method"].append('Naive')
+                oper_char["signal"].append(sig)
+                oper_char["main signal"].append(main_sig)
+                oper_char["prop"].append(prop)
+                oper_char["rho"].append(rho)
+                # oper_char["MSE"].append(MSE_naive)
+                # oper_char["SNR"].append(SNR)
+                oper_char["target"].append(targets[0])
+                pval_dict[rho]['Naive'] += (p_values)
+                oper_char["power"].append(calculate_power(p_values, targets, 0.1))
+                update_targets(dict=target_dict,
+                               true_inter_list=None,
+                               targets=targets, parameter=rho,
+                               method="Naive", idx=idx,
+                               pvals=p_values,
+                               pivots=pivots, sim_idx=i)
+
+            # Continue if Naive yields a nonempty group lasso selection
+            # (this is almost always the case)
+            # Performing data splitting using 'all pairs'
+            # DS solve only
+            ds_rank_def = False
+            (nonzero_ds, selected_groups_ds, subset_select_ds, beta_hat_ds) \
+                = data_splitting_inter(X=design, Y=Y, groups=groups,
+                                       Y_mean=Y_mean, const=const,
+                                       n_features=20, interactions=data_interaction,
+                                       proportion=prop,
+                                       weight_frac=weights, level=0.9,
+                                       mode='weakhierarchy',
+                                       solve_only=True, continued=False,
+                                       parallel=False, p_val=True,
+                                       target_ids=None,
+                                       root_n_scaled=root_n_scaled)
+            if nonzero_ds.sum() + 1 >= n - subset_select_ds.sum():
+                ds_rank_def = True
+                ds_rank_def_count[rho] += 1
+
+            if not ds_rank_def:
+                (coverages_ds, lengths_ds, selected_inter_ds,
+                 p_values_ds, pivots_ds, targets_ds, idx_ds, beta_hat_ds) \
+                    = data_splitting_inter(X=design, Y=Y, groups=groups,
+                                           Y_mean=Y_mean, const=const,
+                                           n_features=20, interactions=data_interaction,
+                                           proportion=prop,
+                                           weight_frac=weights, level=0.9,
+                                           mode='weakhierarchy',
+                                           solve_only=False, continued=True,
+                                           parallel=False, p_val=True,
+                                           target_ids=None,
+                                           root_n_scaled=root_n_scaled,
+                                           subset_cont=subset_select_ds,
+                                           nonzero_cont=nonzero_ds,
+                                           selected_groups_cont=selected_groups_ds,
+                                           soln_cont=beta_hat_ds)
+                noselection_ds = coverages_ds is None
+
+            if not noselection_ds:
+                # Data splitting
+                oper_char["coverage rate"].append(np.mean(coverages_ds))
+                oper_char["avg length"].append(np.mean(lengths_ds))
+                oper_char["method"].append('Data Splitting')
+                oper_char["signal"].append(sig)
+                oper_char["main signal"].append(main_sig)
+                oper_char["prop"].append(prop)
+                oper_char["rho"].append(rho)
+                # oper_char["MSE"].append(MSE_ds)
+                # oper_char["SNR"].append(SNR)
+                oper_char["target"].append(targets_ds[0])
+                pval_dict[rho]['Data Splitting'] += (p_values_ds)
+                oper_char["power"].append(calculate_power(p_values_ds, targets_ds, 0.1))
+                update_targets(dict=target_dict,
+                               true_inter_list=None,
+                               targets=targets_ds, parameter=rho,
+                               method="Data Splitting", idx=idx_ds,
+                               pvals=p_values_ds,
+                               pivots=pivots_ds, sim_idx=i)
+
+            # Continue if data splitting yields a nonempty group lasso selection
+            # (this is almost always the case)
+            # Performing MLE using 'all pairs'
+            (coverages_MLE, lengths_MLE, selected_inter_MLE, p_values_MLE,
+             pivots_MLE, targets_MLE, idx_MLE, beta_hat_MLE) \
+                = (MLE_inference_inter
+                   (X=design, Y=Y, Y_mean=Y_mean, groups=groups,
+                    n_features=p_nl, interactions=data_interaction,
+                    intercept=True,
+                    # randomizer_sd_const=tau,
+                    proportion=prop,
+                    weight_frac=weights,
+                    level=0.9, mode='weakhierarchy', solve_only=False,
+                    continued=False, parallel=False, p_val=True,
+                    target_ids=None,
+                    root_n_scaled=root_n_scaled))
+            noselection_MLE = coverages_MLE is None
+
+            # Collect results if all three methods yields
+            # nonempty first-stage selection
+            if not noselection_MLE:
+                # MLE
+                oper_char["coverage rate"].append(np.mean(coverages_MLE))
+                oper_char["avg length"].append(np.mean(lengths_MLE))
+                oper_char["method"].append('MLE')
+                oper_char["signal"].append(sig)
+                oper_char["main signal"].append(main_sig)
+                oper_char["prop"].append(prop)
+                oper_char["rho"].append(rho)
+                # oper_char["MSE"].append(MSE_MLE)
+                # oper_char["SNR"].append(SNR)
+                oper_char["target"].append(targets_MLE[0])
+                pval_dict[rho]['MLE'] += (p_values_MLE)
+                oper_char["power"].append(calculate_power(p_values_MLE,
+                                                          targets_MLE,
+                                                          0.1))
+                update_targets(dict=target_dict,
+                               true_inter_list=None,
+                               targets=targets_MLE, parameter=rho,
+                               method="MLE", idx=idx_MLE,
+                               pvals=p_values_MLE,
+                               pivots=pivots_MLE, sim_idx=i)
+
+            # Set MSE
+            MSE_naive = np.mean((Y_test - predict(beta_hat_naive, design)) ** 2)
+            MSE_dict["MSE"].append(MSE_naive)
+            MSE_dict["method"].append("Naive")
+            MSE_dict["rho"].append(rho)
+
+            MSE_ds = np.mean((Y_test - predict(beta_hat_ds, design)) ** 2)
+            MSE_dict["MSE"].append(MSE_ds)
+            MSE_dict["method"].append("Data Splitting")
+            MSE_dict["rho"].append(rho)
+
+            MSE_MLE = np.mean((Y_test - predict(beta_hat_MLE, design)) ** 2)
+            MSE_dict["MSE"].append(MSE_MLE)
+            MSE_dict["method"].append("MLE")
+            MSE_dict["rho"].append(rho)
+            MSE_set = True
+
+            joblib.dump([ds_rank_def_count, target_dict, pval_dict, MSE_dict, oper_char],
+                        f'{dir}_{start}_{end}.pkl', compress=1)
+
+if __name__ == '__main__':
+    # Get the script's directory
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    # Change the working directory to the script's directory
+    os.chdir(script_directory)
+    current_directory = os.getcwd()
+    print("Current Working Directory:", current_directory)
+
+    argv = sys.argv
+    ## sys.argv: [something, start, end, ncores]
+    start, end = int(argv[1]), int(argv[2])
+    dir = str(argv[3])
+
+    vary_corr(start, end, dir)
